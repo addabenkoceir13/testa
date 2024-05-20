edff0a95e6eca347a661496ec1a3e840b3af10db
hoskamohamed <addamohamed@hoskadev.com>
2024-05-21 00:22:32 +0100

    create test a

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -28,5 +28,90 @@ class UserSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
+        DB::table('users')->insert([
+            'name' => 'user#01',
+            'email' => 'user01@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'),
+            'roles' => 'user',
+            'status' => 'active',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user#02',
+            'email' => 'user02@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'),
+            'roles' => 'user',
+            'status' => 'active',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user#03',
+            'email' => 'user03@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'),
+            'roles' => 'user',
+            'status' => 'active',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user#04',
+            'email' => 'user04@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'),
+            'roles' => 'user',
+            'status' => 'active',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user#05',
+            'email' => 'user05@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'),
+            'roles' => 'user',
+            'status' => 'inactive',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user#06',
+            'email' => 'user06@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'),
+            'roles' => 'user',
+            'status' => 'inactive',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+        ]);
+
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'user07@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'),
+            'roles' => 'user',
+            'status' => 'active',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+        ]);
     }
 }
